1e1d4e067b3670fb19e5c9e782ef5c397ec3af70
Masongrs <mason@gurustugroup.com>
2015-02-10 09:22:10 -0600

    included 000 in seds remove list

--- auto_checker.sh
+++ auto_checker.sh
@@ -9,7 +9,7 @@ while read file; do
         curl -sL -w "%{http_code} %{url_effective}\\n" "$file" -o /dev/null >> output.txt &
 done < sites.txt;
 
-sed -i "/^\(1\|2\|3\)/d" output.txt;
+sed -i "/^\(0\|1\|2\|3\)/d" output.txt;
 cut -d' ' -f1 output.txt | uniq | grep -f - error_codes.csv >> output.txt;
 sed -i "s/\.$/\.\n/g" output.txt;
 #sed -i "s/[a-zA-z]\,[a-zA-Z]/\n/g" output.txt;
