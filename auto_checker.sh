033dbe69d3d44e4d1c0df84abd0ee986dc70a0a1
Masongrs <mason@gurustugroup.com>
2015-02-09 21:33:03 -0600

    Added functionalty to have the autochecker include http error codes. Have not fully implemented all codes

--- auto_checker.sh
+++ auto_checker.sh
@@ -1,32 +1,40 @@
 #!/bin/bash
 if [ -a output.txt ]
 then
-        rm output.txt;
+	rm output.txt;
 fi
+
 touch output.txt;
 while read file; do
         curl -sL -w "%{http_code} %{url_effective}\\n" "$file" -o /dev/null >> output.txt &
 done < sites.txt;
 
-sed -i '/^\(1\|2\|3\)/d' output.txt;
+sed -i "/^\(1\|2\|3\)/d" output.txt;
+cut -d' ' -f1 output.txt | uniq | grep -f - error_codes.csv >> output.txt;
+sed -i "s/\.$/\.\n/g" output.txt;
+#sed -i "s/[a-zA-z]\,[a-zA-Z]/\n/g" output.txt;
+sed -i "s/\,/\,\ /g" output.txt;
 if [ -s output.txt ]
 then
 
         if [ $1 ]
         then
                 mail -s "Sites Down" $1 < output.txt;
-                rm output.txt
+                #rm output.txt
         else
                 if [ -s email_list.txt ]
                 then
                         while read file; do
                                 mail -s "Sites Down" $file < output.txt;
-                        done < email_list.txt
+                        done < email_list.txt;
                 else
                         :
                 fi
-                rm output.txt
+                #rm output.txt
         fi
 fi      
 
-rm output.txt
+if [ -a output.txt ]
+then
+	rm output.txt;
+fi
