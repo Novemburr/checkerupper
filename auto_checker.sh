0b43a80543110d621de32ea4030695794cb1a1fa
Masongrs <mason@gurustugroup.com>
2015-02-09 19:15:21 -0600

    removed directory refferences for the moment

--- auto_checker.sh
+++ auto_checker.sh
@@ -3,10 +3,10 @@ if [ -a output.txt ]
 then
         rm output.txt;
 fi
-
+touch output.txt;
 while read file; do
         curl -sL -w "%{http_code} %{url_effective}\\n" "$file" -o /dev/null >> output.txt &
-done < ./sites.txt;
+done < sites.txt;
 
 sed -i '/^\(1\|2\|3\)/d' output.txt;
 if [ -s output.txt ]
@@ -14,19 +14,19 @@ then
 
         if [ $1 ]
         then
-                mail -s "Sites Down" $1 < ./output.txt;
-                rm ./output.txt
+                mail -s "Sites Down" $1 < output.txt;
+                rm output.txt
         else
                 if [ -s email_list.txt ]
                 then
                         while read file; do
-                                mail -s "Sites Down" $file < ./output.txt;
-                        done < ./email_list.txt
+                                mail -s "Sites Down" $file < output.txt;
+                        done < email_list.txt
                 else
                         :
                 fi
-                rm ./output.txt
+                rm output.txt
         fi
 fi      
 
-rm ./output.txt
+rm output.txt
