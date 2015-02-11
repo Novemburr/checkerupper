c837086d4b46f26974e89bf79428734510426666
Novemburr <mason.frakes@gmail.com>
2015-02-11 02:16:54 +0000

    Added code to log the number of emails sent per hour and arbitrarily limited them to 3 per hour

--- auto_checker.sh
+++ auto_checker.sh
@@ -16,29 +16,52 @@ sed -i "s/\.$/\.\n/g" output.txt;
 sed -i "s/\,/\,\ /g" output.txt;
 if [ -s output.txt ]
 then
+	echo "$(date +%h)" >> debug.log;
+	COUNT=$(wc -l debug.log);
+	if [ "$COUNT" -lt 4 ]
+	then
 
-        if [ $1 ]
-        then
-                mail -s "Sites Down" $1 < output.txt;
-		echo 'mail sent';
-                #rm output.txt
-        else
-                if [ -s email_list.txt ]
-                then
-                        while read file; do
-                                mail -s "Sites Down" $file < output.txt;
-                        done < email_list.txt;
-                else
-                        :
-                fi
-                #rm output.txt
-        fi
+		if [ $1 ]
+		then
+			mail -s "Sites Down" $1 < output.txt;
+			echo 'mail sent';
+			#rm output.txt
+		else
+			if [ -s email_list.txt ]
+			then
+				while read file; do
+					mail -s "Sites Down" $file < output.txt;
+				done < email_list.txt;
+			else
+				:
+			fi
+			#rm output.txt
+		fi
+	else
+	:
+	fi
 else
 	if [ $1 ]
 	then
 		echo 'empty output';
 	fi
 fi      
+NOW=$(date +%H);
+THEN=$(head -n 1 debug.log)
+if [ "$NOW" == "$THEN" ];
+then
+	:
+else
+	if [ -a debug.log ];
+	then
+		rm debug.log;
+		touch debug.log;
+	else
+		touch badthing.log;
+		echo "Something happened causing debug.log to disapear prematurely" >> badthing.log;
+		touch debug.log;
+	fi
+fi
 
 if [ -a output.txt ]
 then
